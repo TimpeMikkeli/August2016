5467c01b7a1c385c5cd47d2a054cfbcb93cd4c54
Student <Student@MB316-XX>
2016-08-24 12:59:31 +0300

    After first modification

--- CATest1A/Program.cs
+++ CATest1A/Program.cs
@@ -11,6 +11,7 @@ namespace CATest1A
         static void Main(string[] args)
         {
             Console.WriteLine("This is the original one.");
+            Console.WriteLine("This is first modification");
         }
     }
 }
